9f0db77bb91e1a870e258952aef7794cdc61c372
Abdoulaye Ly <abdoulayely148@gmail.com>
2025-10-05 22:14:14 +0000

    ok

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -3,31 +3,31 @@ import fs from "fs";
 import path from "path";
 
 export class MailService {
-  async sendPortfolioMail(data: { nom: string; email: string; message: string }) {
-    const templatePath = path.resolve("src/templates/mail.html");
-    let html = fs.readFileSync(templatePath, "utf8");
+    async sendPortfolioMail(data: { nom: string; email: string; message: string }) {
+        const templatePath = path.resolve("src/templates/mail.html");
+        let html = fs.readFileSync(templatePath, "utf8");
 
-    // Remplace les placeholders
-    html = html
-      .replace("{{nom}}", data.nom)
-      .replace("{{email}}", data.email)
-      .replace("{{message}}", data.message);
+        // Remplace les placeholders
+        html = html
+            .replace("{{nom}}", data.nom)
+            .replace("{{email}}", data.email)
+            .replace("{{message}}", data.message);
 
-    // ✅ Transporteur SendGrid
-    const transporter = nodemailer.createTransport({
-      service: "SendGrid",
-      auth: {
-        api_key: process.env.SENDGRID_API_KEY!,
-      } as any,
-    });
+        // ✅ Transporteur SendGrid
+        const transporter = nodemailer.createTransport({
+            service: "SendGrid",
+            auth: {
+                api_key: process.env.SENDGRID_API_KEY!,
+            } as any,
+        });
 
-    await transporter.sendMail({
-      from: `"Portfolio Abdoulaye" <no-reply@tondomaine.com>`,
-      to: process.env.TO_EMAIL,
-      subject: "📩 Nouveau message depuis ton portfolio",
-      html,
-    });
+        await transporter.sendMail({
+            from: `"Portfolio Abdoulaye" <no-reply@tondomaine.com>`,
+            to: process.env.TO_EMAIL,
+            subject: "📩 Nouveau message depuis ton portfolio",
+            html,
+        });
 
-    console.log("✅ Mail envoyé avec succès via SendGrid");
-  }
+        console.log("✅ Mail envoyé avec succès via SendGrid");
+    }
 }
