fa366891e3397beba9730fa32ec8b5135655573d
Abdoulaye Ly <abdoulayely148@gmail.com>
2025-10-06 11:45:51 +0000

    probleme cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
 import mailRoutes from "./routes/mail.routes.js";
+import cors from 'cors';
+
 
 dotenv.config();
 console.log("Variables d'environnement chargées");
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: function(origin, callback) {
+    // Autoriser les requêtes sans origine (comme les appels d'API mobile ou Postman)
+    if (!origin) return callback(null, true);
+    
+    // Liste des origines autorisées
+    const allowedOrigins = ['https://abdoulaye-lydev.vercel.app/'];
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    
+    callback(new Error('Not allowed by CORS'));
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 console.log("Middleware JSON configuré");
 
 // Routes
